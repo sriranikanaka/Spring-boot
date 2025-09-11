14ee303f08b9db5ea2c6b05279e455eb9b5e515e
sriranikanaka <ssrkr232001@gmail.com>
2025-09-11 15:21:23 +0530

    Create GlobalExceptionHandler.java

---
+++ section 10/RestApi Exception handling/70. Handing Specific Custom Exception - ResourceNotFoundException/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+Before implementing this comment the exception code in the usercontroller file
+  implement this class in error package
+
+package Net.javaguides.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+                                                                        WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "USER_NOT_FOUND"
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+}
